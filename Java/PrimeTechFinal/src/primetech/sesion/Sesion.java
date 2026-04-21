751bf3c53193b358463e3ab20b2e25d6ac3ed1a6
Joseluyea <dpjoselu@gmail.com>
2026-04-21 10:51:41 +0200

    Añado proyecto de java, lo he estructurado por paquetes, con una estructura encapsulada y logica DAO, además de un desarrollo basado en la seguridad, incluyendo utilizades de cifrado/hasheado de contraseñas en la base de datos

---
+++ Java/PrimeTechFinal/src/primetech/sesion/Sesion.java
@@ -0,0 +1,30 @@
+package primetech.sesion;
+
+import primetech.model.Empleado;
+
+public class Sesion {
+
+    private static Empleado empleadoActual = null;
+
+    private Sesion() {}
+
+    public static void iniciar(Empleado emp) {
+        empleadoActual = emp;
+    }
+
+    public static void cerrar() {
+        empleadoActual = null;
+    }
+
+    public static Empleado getEmpleado() {
+        return empleadoActual;
+    }
+
+    public static boolean haySession() {
+        return empleadoActual != null;
+    }
+
+    public static boolean esAdmin() {
+        return empleadoActual != null && "admin".equals(empleadoActual.getCargo());
+    }
+}
