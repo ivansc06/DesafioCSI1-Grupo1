5fde37a716aa1482fb5ab32625a22cb52e09017b
Joseluyea <dpjoselu@gmail.com>
2026-05-01 02:18:23 +0200

    Ya he solucionado el error de las pools cambiando los metodos de clases dao, ya que los habia diseñado para una conexion normal. Tambeien he añadido flatlab para una mejor ui, efectivamente se ve bastante mejor

--- Java/PrimeTechFinal/src/primetechfinal/PrimeTech.java
+++ Java/PrimeTechFinal/src/primetechfinal/PrimeTech.java
@@ -1,10 +1,12 @@
 package primetechfinal;
 
+import com.formdev.flatlaf.FlatIntelliJLaf;
 import javax.swing.SwingUtilities;
 
 public class PrimeTech {
 
     public static void main(String[] args) {
+        FlatIntelliJLaf.setup();
         SwingUtilities.invokeLater(() -> {
             new LoginFrame().setVisible(true); //forma de abrir pantalla gastando menos memoria
         });
