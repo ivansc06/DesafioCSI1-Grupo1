4476473422534e09a1c36f6636e5ed2ff6d07278
Joseluyea <dpjoselu@gmail.com>
2026-05-06 22:06:02 +0200

    He eliminado el tabbedpanel. Ahora se navega con unos eventos que he puesto en labels, en la foto derecha. Queda todo mucho mas profesional. He terminado de añadir iconos. Practicamente esta finalizada

--- Java/PrimeTechFinal/src/primetechfinal/model/Producto.java
+++ Java/PrimeTechFinal/src/primetechfinal/model/Producto.java
@@ -21,20 +21,92 @@ public class Producto {
         this.stock        = stock;
     }
 
-    public int    getIdProducto()   { return idProducto; }
-    public String getNombre()       { return nombre; }
-    public String getDescripcion()  { return descripcion; }
-    public double getPrecioCompra() { return precioCompra; }
-    public double getPrecioVenta()  { return precioVenta; }
-    public int    getStock()        { return stock; }
-
-    public void setIdProducto(int idProducto)       { this.idProducto = idProducto; }
-    public void setNombre(String nombre)             { this.nombre = nombre; }
-    public void setDescripcion(String descripcion)   { this.descripcion = descripcion; }
-    public void setPrecioCompra(double precioCompra) { this.precioCompra = precioCompra; }
-    public void setPrecioVenta(double precioVenta)   { this.precioVenta = precioVenta; }
-    public void setStock(int stock)                  { this.stock = stock; }
+    
 
     @Override
-    public String toString() { return nombre; }
+    public String toString() { return getNombre(); }
+
+    /**
+     * @return the idProducto
+     */
+    public int getIdProducto() {
+        return idProducto;
+    }
+
+    /**
+     * @param idProducto the idProducto to set
+     */
+    public void setIdProducto(int idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the descripcion
+     */
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    /**
+     * @param descripcion the descripcion to set
+     */
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    /**
+     * @return the precioCompra
+     */
+    public double getPrecioCompra() {
+        return precioCompra;
+    }
+
+    /**
+     * @param precioCompra the precioCompra to set
+     */
+    public void setPrecioCompra(double precioCompra) {
+        this.precioCompra = precioCompra;
+    }
+
+    /**
+     * @return the precioVenta
+     */
+    public double getPrecioVenta() {
+        return precioVenta;
+    }
+
+    /**
+     * @param precioVenta the precioVenta to set
+     */
+    public void setPrecioVenta(double precioVenta) {
+        this.precioVenta = precioVenta;
+    }
+
+    /**
+     * @return the stock
+     */
+    public int getStock() {
+        return stock;
+    }
+
+    /**
+     * @param stock the stock to set
+     */
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
 }
